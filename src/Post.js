2f42ebc6d47ae6944cf6fa76e1914409f16d9005
jeemyeong <jeemyeong@gmail.com>
2017-07-22 13:06:25 +0900

    Design simple for post with semantic

--- src/Post.js
+++ src/Post.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image } from 'semantic-ui-react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
 class Post extends Component {
   render() {
@@ -8,22 +8,22 @@ class Post extends Component {
       return null;
     }
     const parsedPosts = posts.map((post, index)=>
-      <ul key={index}>
-        <li>
-          date: {post.date.toDateString()}
-        </li>
-        <li>
-          text: {post.text}
-        </li>
-        <li>
-          photoUrls: {post.photoUrls.map((photoUrl, index) =>
-            <Image src={photoUrl} size='small' key={index}/>
-          )}
-        </li>
-        <li>
-          writter: {post.writter}
-        </li>
-      </ul>
+      <Card key={index}>
+        {post.photoUrls.map((photoUrl, index) =>
+          <Image src={photoUrl} key={index}/>
+        )}
+        <Card.Content>
+          <Card.Header>{post.writter}</Card.Header>
+          <Card.Meta>{post.date.toDateString()}</Card.Meta>
+          <Card.Description>{post.text}</Card.Description>
+        </Card.Content>
+        {/* <Card.Content extra>
+          <a>
+            <Icon name='user' />
+            10 Friends
+          </a>
+        </Card.Content> */}
+      </Card>
     );
     return (
       <div>
