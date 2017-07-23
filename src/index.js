7021e1a5dcd7ec319fe30cc21d74030eb426e3f3
jeemyeong <jeemyeong@gmail.com>
2017-07-23 12:50:56 +0900

    Add mobx in index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import './reactDayPickerStyle.css';
+import { useStrict } from 'mobx';
+import { Provider } from 'mobx-react';
+import postStore from './stores/postStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+useStrict(true);
+
+ReactDOM.render(
+  <Provider postStore={postStore} >
+    <App />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
